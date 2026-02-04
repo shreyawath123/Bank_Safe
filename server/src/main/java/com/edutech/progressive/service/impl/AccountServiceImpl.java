28ed4c6de1ab87bc29d28c36483f51ed792c55e0
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:30:41 +0530

    Update AccountServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImpl.java
@@ -1,7 +1,55 @@
 package com.edutech.progressive.service.impl;
 
+import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AccountServiceImpl  {
+import com.edutech.progressive.dao.AccountDAO;
+import com.edutech.progressive.dao.AccountDAOImpl;
+import com.edutech.progressive.entity.Accounts;
+import com.edutech.progressive.service.AccountService;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountDAO accountDAO;
+
+    public AccountServiceImpl(AccountDAOImpl accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() {
+        return accountDAO.getAllAccounts();
+    }
+
+    public Accounts getAccountById(int accountId) {
+        return accountDAO.getAccountById(accountId);
+    }
+
+    @Override
+    public int addAccount(Accounts accounts) {
+        return accountDAO.addAccount(accounts);
+    }
+
+    public void updateAccount(Accounts accounts) {
+        accountDAO.updateAccount(accounts);
+    }
+
+    public void deleteAccount(int accountId) {
+        accountDAO.deleteAccount(accountId);
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsSortedByBalance() {
+        List<Accounts> ans = accountDAO.getAllAccounts();
+        Collections.sort(ans);
+        return ans;
+    }
+
+    public List<Accounts> getAccountsByUser(int userId) {
+        return accountDAO.getAllAccountsByCustomer(userId);
+    }
 
 }
