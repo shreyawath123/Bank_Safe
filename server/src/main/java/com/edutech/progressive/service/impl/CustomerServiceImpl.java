365d50cfa2e267189e59dca2fa97b15306857f24
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 12:29:32 +0530

    Update CustomerServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
@@ -1,48 +1,45 @@
 package com.edutech.progressive.service.impl;
 
+import java.sql.SQLException;
 import java.util.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.dao.CustomerDAO;
 import com.edutech.progressive.dao.CustomerDAOImpl;
 import com.edutech.progressive.entity.Customers;
 import com.edutech.progressive.service.CustomerService;
 
-@Service
 public class CustomerServiceImpl implements CustomerService {
-    @Autowired
     private CustomerDAO customerDAO;
+    private static List<Customers> customersList = new ArrayList<>();
 
     public CustomerServiceImpl(CustomerDAOImpl customerDAO) {
         this.customerDAO = customerDAO;
     }
 
     @Override
-    public List<Customers> getAllCustomers() {
+    public List<Customers> getAllCustomers() throws SQLException {
         return customerDAO.getAllCustomers();
     }
 
-    public Customers getCustomerById(int customerId) {
+    public Customers getCustomerById(int customerId) throws SQLException {
         return customerDAO.getCustomerById(customerId);
     }
 
     @Override
-    public int addCustomer(Customers customers) {
+    public int addCustomer(Customers customers) throws SQLException {
         return customerDAO.addCustomer(customers);
     }
 
-    public void updateCustomer(Customers customers) {
+    public void updateCustomer(Customers customers) throws SQLException {
         customerDAO.updateCustomer(customers);
     }
 
-    public void deleteCustomer(int customerId) {
+    public void deleteCustomer(int customerId) throws SQLException {
         customerDAO.deleteCustomer(customerId);
     }
 
     @Override
-    public List<Customers> getAllCustomersSortedByName() {
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
         List<Customers> ans = customerDAO.getAllCustomers();
         Collections.sort(ans);
         return ans;
