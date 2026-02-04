8631ea57d3c42af02b4379f118979141fd097ce5
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 12:30:12 +0530

    Update CustomerServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
@@ -1,31 +1,29 @@
 package com.edutech.progressive.service.impl;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.edutech.progressive.entity.Customers;
 import com.edutech.progressive.service.CustomerService;
 
-@Service
 public class CustomerServiceImplArraylist implements CustomerService {
     private static List<Customers> customersList = new ArrayList<>();
 
     @Override
-    public List<Customers> getAllCustomers() {
+    public List<Customers> getAllCustomers() throws SQLException {
         return customersList;
     }
 
     @Override
-    public int addCustomer(Customers customers) {
+    public int addCustomer(Customers customers) throws SQLException {
         customersList.add(customers);
         return customersList.size();
     }
 
     @Override
-    public List<Customers> getAllCustomersSortedByName() {
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
         Collections.sort(customersList);
         return customersList;
     }
