7f7e4460c5d997bf726e385e8190faf84446f496
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 12:27:30 +0530

    Update BankSafeApplication.java

--- server/src/main/java/com/edutech/progressive/BankSafeApplication.java
+++ server/src/main/java/com/edutech/progressive/BankSafeApplication.java
@@ -1,24 +1,28 @@
 package com.edutech.progressive;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.edutech.progressive.config.DatabaseConnectionManager;
+import com.edutech.progressive.dao.AccountDAOImpl;
+import com.edutech.progressive.dao.CustomerDAOImpl;
+import com.edutech.progressive.entity.Accounts;
+import com.edutech.progressive.entity.Customers;
+import com.edutech.progressive.service.AccountService;
+import com.edutech.progressive.service.CustomerService;
+import com.edutech.progressive.service.impl.AccountServiceImpl;
+import com.edutech.progressive.service.impl.CustomerServiceImpl;
 
-@SpringBootApplication
 public class BankSafeApplication {
-
     public static void main(String[] args) throws SQLException {
-        // Connection conn = DatabaseConnectionManager.getConnection();
-        // System.out.println("Welcome to BankSafe Project!");
-        // System.out.println(conn);
-        // AccountDAOImpl accountDao = new AccountDAOImpl();
-        // AccountService as = new AccountServiceImpl(accountDao);
-        // CustomerDAOImpl customerDAO = new CustomerDAOImpl();
-        // CustomerService cs = new CustomerServiceImpl(customerDAO);
-        // Accounts account = new Accounts(22, 1111);
-        // Customers customer = new Customers("abs", "DD@gmail.com", "ddabs",
-        // "password");
-        SpringApplication.run(BankSafeApplication.class, args);
+        Connection conn = DatabaseConnectionManager.getConnection();
+        System.out.println("Welcome to BankSafe Project!");
+        System.out.println(conn);
+        AccountDAOImpl accountDao = new AccountDAOImpl();
+        AccountService as = new AccountServiceImpl(accountDao);
+        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+        CustomerService cs = new CustomerServiceImpl(customerDAO);
+        Accounts account = new Accounts(22, 1111);
+        Customers customer = new Customers("abs", "DD@gmail.com", "ddabs", "password");
     }
 }
