4e215c066c09c74f901c65f6e68d233a8dcda2c4
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:21:02 +0530

    Update Accounts.java

--- server/src/main/java/com/edutech/progressive/entity/Accounts.java
+++ server/src/main/java/com/edutech/progressive/entity/Accounts.java
@@ -1,5 +1,68 @@
 package com.edutech.progressive.entity;
 
-public class Accounts {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Accounts implements Comparable<Accounts> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int accountId;
+    private int customerId;
+    private double balance;
+
+    public Accounts() {
+    }
+
+    public Accounts(double balance) {
+        this.balance = balance;
+    }
+
+    public Accounts(int customerId, double balance) {
+        this.customerId = customerId;
+        this.balance = balance;
+    }
+
+    public Accounts(int accountId, int customerId, double balance) {
+        this.accountId = accountId;
+        this.customerId = customerId;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public int compareTo(Accounts o) {
+        return Double.compare(this.getBalance(), o.getBalance());
+    }
+
+    @Override
+    public String toString() {
+        return "Accounts [accountId=" + accountId + ", customerId=" + customerId + ", balance=" + balance + "]";
+    }
 
 }
