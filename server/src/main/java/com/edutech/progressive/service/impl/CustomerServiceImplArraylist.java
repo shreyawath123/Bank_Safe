8f5d64ae72d224d3e2a509e2528f0630150fb6fe
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:32:00 +0530

    Update CustomerServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
@@ -1,5 +1,37 @@
 package com.edutech.progressive.service.impl;
 
-public class CustomerServiceImplArraylist {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Customers;
+import com.edutech.progressive.service.CustomerService;
+
+@Service
+public class CustomerServiceImplArraylist implements CustomerService {
+    private static List<Customers> customersList = new ArrayList<>();
+
+    @Override
+    public List<Customers> getAllCustomers() {
+        return customersList;
+    }
+
+    @Override
+    public int addCustomer(Customers customers) {
+        customersList.add(customers);
+        return customersList.size();
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() {
+        Collections.sort(customersList);
+        return customersList;
+    }
+
+    public void emptyArrayList() {
+        customersList = new ArrayList<>();
+    }
 
 }
