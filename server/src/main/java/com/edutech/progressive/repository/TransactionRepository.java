1012ce0a12da13cec98d14cef5d4629cc5f2810f
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 12:25:09 +0530

    Update TransactionRepository.java

--- server/src/main/java/com/edutech/progressive/repository/TransactionRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/TransactionRepository.java
@@ -1,8 +1,4 @@
 package com.edutech.progressive.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.progressive.entity.Transactions;
-
-public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
+public interface TransactionRepository  {
 }
