091105d0cf1b37f96a86a8c20c020f110d3db6fe
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:23:44 +0530

    Update CustomerDAO.java

--- server/src/main/java/com/edutech/progressive/dao/CustomerDAO.java
+++ server/src/main/java/com/edutech/progressive/dao/CustomerDAO.java
@@ -1,16 +1,16 @@
 package com.edutech.progressive.dao;
 
-
+import java.sql.SQLException;
 import java.util.List;
 
 import com.edutech.progressive.dto.CustomerAccountInfo;
 import com.edutech.progressive.entity.Customers;
 
 public interface CustomerDAO {
-    int addCustomer(Customers customers);
-    Customers getCustomerById(int customerId);
-    void updateCustomer(Customers customers);
-    void deleteCustomer(int customerId);
-    List<Customers> getAllCustomers();
-    CustomerAccountInfo getCustomerAccountInfo(int customerId);
+    public Customers getCustomerById(int customerId);
+    public int addCustomer(Customers customers);
+    public void updateCustomer(Customers customers);
+    public void deleteCustomer(int customerId);
+    public CustomerAccountInfo getCustomerAccountInfo(int customerId) throws SQLException;
+    public List<Customers> getAllCustomers();
 }
