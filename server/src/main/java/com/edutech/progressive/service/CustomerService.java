404ffc149604953143ada0b1061c727ebeaecbf2
shreyawath123 <shreyawath123@gmail.com>
2026-02-05 15:31:05 +0530

    Update CustomerService.java

--- server/src/main/java/com/edutech/progressive/service/CustomerService.java
+++ server/src/main/java/com/edutech/progressive/service/CustomerService.java
@@ -6,23 +6,23 @@ import java.util.List;
 import com.edutech.progressive.entity.Customers;
 
 public interface CustomerService {
-    public List<Customers> getAllCustomers()throws SQLException;
+    public List<Customers> getAllCustomers() throws SQLException;
 
-    public int addCustomer(Customers customers)throws SQLException;
+    public int addCustomer(Customers customers) throws SQLException;
 
-    public List<Customers> getAllCustomersSortedByName()throws SQLException;
+    public List<Customers> getAllCustomersSortedByName() throws SQLException;
 
-    default public void emptyArrayList()throws SQLException {
+    default public void emptyArrayList() throws SQLException {
     }
 
     // Do not implement these methods in CustomerServiceImplArraylist.java class
-    default void updateCustomer(Customers customers)throws SQLException {
+    default void updateCustomer(Customers customers) throws SQLException {
     }
 
-    default void deleteCustomer(int customerId)throws SQLException {
+    default void deleteCustomer(int customerId) throws SQLException {
     }
 
-    default Customers getCustomerById(int customerId)throws SQLException {
+    default Customers getCustomerById(int customerId) throws SQLException {
         return null;
     }
 
